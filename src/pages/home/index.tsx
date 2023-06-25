d516ce5ab97b864ab880583451e407bd12fe2e70
Tuwan <ping.zhang@xuncetech.com>
2023-06-25 17:45:49 +0800

    fix: 批量更新时的触发情况

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div style={{paddingLeft:'10px'}}>
-      <Link to="/batchUpdate">批量更新</Link><br />
+      <Link to="/batchUpdate">批量更新时的触发情况</Link><br />
     </div>
   )
 }
